ae880f6e525279046905db40736facb632c9a933
sofyahrek <sofigrekal@gmail.com>
2023-06-22 19:49:26 +0200

    corrected code formatting in Header block

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,5 +1,7 @@
 import "./style.css";
 
-const Header = ({headerName}) => (<h1 className="header">{headerName}</h1>);
+const Header = ({headerName}) => (
+<h1 className="header">{headerName}</h1>
+);
 
 export default Header;
