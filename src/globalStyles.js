21bddf693e5ea767447705ec5ed5012fba2fa1cd
sofyahrek <sofigrekal@gmail.com>
2023-09-28 20:00:19 +0200

    feat: added global styles with styled-components

---
+++ src/globalStyles.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+  *,
+  ::after,
+  ::before {
+    box-sizing: inherit;
+  }
+
+  body {
+    background-color: #ddd;
+    max-width: 900px;
+    font-family: "Lato", sans-serif;
+    margin: 0 auto;
+  }
+
+  button:hover{cursor: pointer}
+  `;
