6b0df34394f5a4561744f67ac27bcc0fec75ca59
sofyahrek <sofigrekal@gmail.com>
2024-03-24 22:12:47 +0100

    feat: updated search to dynamically handle query params

--- src/features/tasks/SearchBar.js
+++ src/features/tasks/SearchBar.js
@@ -1,9 +1,36 @@
+import { useLocation, useNavigate } from "react-router-dom";
+
 import { StyledForm, Input } from "./StyledFormComponents";
 
 function SearchBar() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const searchParams = new URLSearchParams(location.search);
+  const query = searchParams.get("search");
+
+  const updateSearchParams = (searchTerm ) => {
+    if (searchTerm ) {
+      searchParams.set("search", searchTerm );
+    } else {
+      searchParams.delete("search");
+    }
+
+    navigate(`${location.pathname}?${searchParams.toString()}`, {
+      replace: true,
+    });
+  };
+
   return (
     <StyledForm>
-      <Input placeholder="Filtruj zadania" type="text" />
+      <Input
+        placeholder="Filtruj zadania"
+        type="text"
+        value={query || ""}
+        onChange={(event) => {
+          updateSearchParams(event.target.value);
+        }}
+      />
     </StyledForm>
   );
 }
