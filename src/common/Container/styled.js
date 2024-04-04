c78612673106fd12709a50832ab2caf1c83f1498
sofyahrek <sofigrekal@gmail.com>
2024-04-04 18:37:57 +0200

    style: greater margin on mobile devices

--- src/common/Container/styled.js
+++ src/common/Container/styled.js
@@ -2,5 +2,8 @@ import styled from "styled-components";
 
 export const StyledContainer = styled.main`
   max-width: 900px;
-  margin: 50px auto;
+  margin: 80px auto;
+  @media (max-width: ${({ theme }) => theme.media.mobile}px) {
+    margin: 110px auto;
+  }
 `;
