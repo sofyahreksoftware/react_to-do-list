a3c4fee833569343494bf3e2ffc1ed47bbc373ee
sofyahrek <sofigrekal@gmail.com>
2024-04-06 15:40:57 +0200

    style: added an extra line between burger button and navigation

--- src/common/Navigation/index.js
+++ src/common/Navigation/index.js
@@ -15,6 +15,7 @@ export const Navigation = () => {
         <BurgerBar />
         <BurgerBar />
       </BurgerButton>
+      
       <Nav $visible={navVisible}>
         <NavList>
           <StyledNavLink
