e47a3d30ac7fedc8e279a09dba5732937204a175
sofyahrek <sofigrekal@gmail.com>
2024-04-07 17:22:54 +0200

    feat: reduce header side margin on mobile devices

--- src/common/Header/styled.js
+++ src/common/Header/styled.js
@@ -3,4 +3,8 @@ import styled from "styled-components";
 export const StyledHeader = styled.h1`
   margin: 20px 40px;
   font-weight: 700;
+
+  @media (max-width: ${({ theme }) => theme.media.mobile}px) {
+    margin: 20px 19px;
+  }
 `;
