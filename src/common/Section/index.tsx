8c1a2fa77d133a76383aae930ceb2b07af1e7685
Sofya <sofigrekal@gmail.com>
2024-10-24 08:47:54 +0200

    feat: migrate components from common folder to TypeScript

--- src/common/Section/index.tsx
+++ src/common/Section/index.tsx
@@ -1,10 +1,17 @@
+import { ReactElement, ReactNode } from "react";
 import { StyledSection, SectionHeader, Title } from "./styled";
+
+interface SectionProps {
+  headerAdditionalContent?: ReactElement;
+  sectionTitle: string;
+  sectionBody?: ReactNode;
+}
 
 export const Section = ({
   headerAdditionalContent,
   sectionTitle,
   sectionBody,
-}) => (
+}: SectionProps) => (
   <StyledSection>
     <SectionHeader>
       <Title>{sectionTitle}</Title>
